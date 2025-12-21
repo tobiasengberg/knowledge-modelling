4740008f2867d71dce5124d40a4d06b75f1f317c
tobiasengberg <tobias.engberg@exilic.se>
2025-12-21 17:16:27 +0100

    Setup for tabular data trials

---
+++ tabular-data/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import FirstTable from "./pages/table1/FirstTable.tsx";
+
+function App() {
+
+  return (
+    <>
+      <FirstTable />
+    </>
+  )
+}
+
+export default App
---
+++ tabular-data/src/pages/table1/FirstTable.tsx
@@ -0,0 +1,10 @@
+
+export const FirstTable = () => {
+    return (
+        <div>
+            <h1>First table</h1>
+        </div>
+    );
+};
+
+export default FirstTable;
